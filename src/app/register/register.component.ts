7a2a7bd6851d392024ce3290e015f78f8a2a5488
chame <angelchame6@gmail.com>
2024-10-20 13:06:18 -0600

    agregando en el registro responsabilidad

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,36 +1,33 @@
-import { Component} from '@angular/core';
-import { FormGroup,FormBuilder } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { DataService } from '../service/data.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css'] // Corrige styleUrl a styleUrls
 })
 export class RegisterComponent {
+  grupo: FormGroup;
 
-grupo : FormGroup
+  constructor(private fb: FormBuilder, private data: DataService) {
+    this.grupo = this.fb.group({
+      nombre: [''],
+      correo: [''],
+      password: [''],
+      tipo: ['usuario'], // Tipo por defecto
+    });
+  }
 
-constructor(private fb:FormBuilder, private data: DataService){
-  this.grupo = this.fb.group({
-    nombre:[''],
-    correo: [''],
-    password: [''],
-  })
-}
+  envioDatos() {
+    this.data.addData(this.grupo.value);
 
-envioDatos() {
-  
-  this.data.addData(this.grupo.value);
-  
-  // Muestra en consola los datos que se están enviando
-  console.log('Datos enviados:', this.grupo.value);
-  
-  // Muestra en consola los datos almacenados en el servicio
-  console.log('Datos almacenados:', this.data.getData());
-  
-  
-  this.grupo.reset();
-}
+    // Muestra en consola los datos que se están enviando
+    console.log('Datos enviados:', this.grupo.value);
 
+    // Muestra en consola los datos almacenados en el servicio
+    console.log('Datos almacenados:', this.data.getData());
+
+    this.grupo.reset();
+  }
 }
